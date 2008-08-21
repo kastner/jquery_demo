ee56bc8cb9506cc26d6f4494c70edbba332c92d5
Erik Kastner <kastner@gmail.com>
2008-08-21 10:23:48 -0400

    Updating files to show editing, and before_filters

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
-  before_filter :get_book, :only => [:show, :edit, :update, :destroy]
-  before_filter :get_authors, :only => [:new, :edit, :create]
+  before_filter :get_book, :only => %w|show edit update destroy|
+  before_filter :get_authors, :only => %w|new edit create|
+  before_filter :fetch_author_from_params, :only => %w|create update|
   
   # GET /books
   def index
@@ -9,6 +10,7 @@ class BooksController < ApplicationController
 
   # GET /books/1
   def show
+    # @book populated via the before_filter
   end
 
   # GET /books/new
@@ -18,25 +20,25 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    # @book populated via the before_filter
   end
 
   # POST /books
   def create
-    @book = Book.new(params[:book])
-    author = Author.find_by_name(params[:author])
-    if (!author)
-      flash[:notice] = "I don't know the author '#{params[:author]}'"
-      render :action => "new"
-    else
-      @book.author = author
-      @book.save!
+    @book = Book.new(params[:book])    
+    @book.author = @author
+      
+    if (@book.save)
       flash[:notice] = 'Book was successfully created.'
       redirect_to(@book)
+    else
+      render :action => "new"
     end
   end
 
   # PUT /books/1
   def update
+    params[:book].merge!({"author" => @author})
     if @book.update_attributes(params[:book])
       flash[:notice] = 'Book was successfully updated.'
       redirect_to(@book)
@@ -59,5 +61,13 @@ private
   
   def get_authors
     @authors = Author.find(:all)
+    if (!@author)
+      flash[:notice] = "I don't know the author '#{params[:author]}'"
+      render :action => "new" && return
+    end
+  end
+  
+  def fetch_author_from_params
+    @author = Author.find_by_name(params[:author])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
   belongs_to :author
+  
+  validates_uniqueness_of :title
+  validates_length_of :title, :within => 5..120
 end
